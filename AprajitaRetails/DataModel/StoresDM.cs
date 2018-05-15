cb39cea2f0de85e975385f128983f7d839e2662d
Amit <Amit@AMITKUMAR_LP>
2018-05-15 11:32:01 +0530

    Comitied from XPS

---
+++ AprajitaRetails/DataModel/StoresDM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AprajitaRetails.DataModel
+{
+    /// <summary>
+    /// TableName:Stores
+    /// </summary>
+    class StoresDM
+    {
+        public int _ID { get; set; }
+        public string StoreName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string PinCode { get; set; }
+        public string PhoneNo { get; set; }
+        public string StoreManagerName { get; set; }
+        public string StoreManagerPhoneNo { get; set; }
+        public string GSTNO { get; set; }
+        public int NoOfEmployees { get; set; }
+        public DateTime OpeningDate { get; set; }
+        public DateTime ClosingDate { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ AprajitaRetails/ViewModel/StoresVM.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AprajitaRetails.Data;
+
+namespace AprajitaRetails.ViewModel
+{
+    class StoresVM
+    {
+        StoresBD sDB;
+        public StoresVM()
+        {
+            sDB = new StoresBD ();
+        }
+        public List<Stores> GetStores()
+        {
+            throw new NotImplementedException ();
+        }
+        public int GetStoresId()
+        {
+            throw new NotImplementedException ();
+        }
+        public int GetStoresId(string city)
+        {
+            throw new NotImplementedException ();
+        }
+        public Stores GetStores(int id)
+        {
+            throw new NotImplementedException ();
+        }
+        public Stores GetStores (string storename,string City)
+        {
+            throw new NotImplementedException ();
+        }
+        public Stores GetStores(string City)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public int AddStores(Stores store)
+        {
+            return sDB.InsertData (store);
+        }
+        public int DelStores(int id)
+        {
+            //TODO: in Larger prospective
+            throw new NotImplementedException ();
+        }
+        public int UpdateStores(int id, Stores store)
+        {
+            throw new NotImplementedException ();
+        }
+
+
+    }
+    class StoresBD : DataOps<Stores>
+    {
+        public override int InsertData(Stores obj)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public override Stores ResultToObject(List<Stores> data, int index)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public override Stores ResultToObject(SortedDictionary<string, string> data)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public override List<Stores> ResultToObject(List<SortedDictionary<string, string>> dataList)
+        {
+            throw new NotImplementedException ();
+        }
+    }
+}
